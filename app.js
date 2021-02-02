e6b93902a5b70c4e83adeede2f183085c81ee11c
einbulinda <einbulinda@gmail.com>
2021-02-02 12:54:41 +0300

    access token

--- app.js
+++ app.js
@@ -1,76 +1,42 @@
-const express = require('express');
-const request = require('request');
+const express = require("express");
+var request = require("request");
 const app = express();
 
-//routes
-app.get('/', (req,res)=>{
-    res.send("You're home, welcome")
-})
+// routes
+app.get("/", (req, res) => {
+  res.send("You're home. Welcome!");
+});
 
-app.get('/access_token',access, (req,res)=>{
-    res.status(200).json({access_token: req.access_token})
-})
+app.get("/access_token", (req, res) => {
+  //access token
 
-app.get('/register', access, (req, resp)=>{
-    let url = 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl'
-    let auth = "Bearer " + req.access_token;
+  var request = require("request"),
+    consumer_key = "k4GSFax0gdSONmQpuAnXZde23fsHuGt2",
+    consumer_secret = "BhSJBDUmIwlup98h",
+    url =
+      "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
+  auth =
+    "Basic " +
+    new Buffer.from(consumer_key + ":" + consumer_secret).toString("base64");
 
-    request(
-        {
-            url: url,
-            method: 'POST',
-            headers:{
-                'Authorization': auth
-            },
-            json:{
-                 "ShortCode": "174379",
-                "ResponseType": "Complete",
-                "ConfirmationURL": "http://8000-af3b3cc2-a932-4b2d-b99f-8627725c54d9.ws-eu03.gitpod.io/confirmation",
-                "ValidationURL": "http://8000-af3b3cc2-a932-4b2d-b99f-8627725c54d9.ws-eu03.gitpod.io/validation_url"
-            }
-        },
-        function(error, response, body){
-            if(error){console.log(error)}
-            resp.status(200).json(body)
-        }
-    )
-})
-
-
-
-
-
-
-
-
-
-
-
-function access(req, res, next){
-    // access token
-    let url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
-    let auth =new Buffer('k4GSFax0gdSONmQpuAnXZde23fsHuGt2:BhSJBDUmIwlup98h').toString("base64");
-
-    request({
-        url:url,
-        headers:{
-            "Authorization": "Basic " + auth
-        }
-    },(error, response, body)=>{
-        if(error){
-            console.log(error)
-        }else{
-            // response
-            req.access_token = JSON.parse(body).access_token
-            next()
-        }
-    })
-}
+  request(
+    {
+      url: url,
+      headers: {
+        Authorization: auth,
+      },
+    },
+    function (error, response, body) {
+      // TODO: Use the body object to extract OAuth access token
+    }
+  );
+});
 
 // listen
-app.listen(8000, (err, live)=>{
-    if(err){
-        console.error(err)
-    }
-    console.log("Server running at port 8000")
+const PORT = 80;
+app.listen(PORT, (err, live) => {
+  if (err) {
+    console.error(err);
+  }
+  console.log("Server is running at " + PORT);
 });
